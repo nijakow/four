a5cd215d2d8a511e851586a76832fffc5354041a
nijakow <79372954+nijakow@users.noreply.github.com>
2021-12-15 23:00:13 +0100

    Added networking capabilities

---
+++ src/nijakow/four/net/Server.java
@@ -0,0 +1,61 @@
+package nijakow.four.net;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.StandardSocketOptions;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.Set;
+
+public class Server {
+	private final Selector selector;
+
+	public Server() throws IOException {
+		this.selector = Selector.open();
+	}
+	
+	public void serveOn(int port) throws IOException {
+		ServerSocketChannel serverSocket = ServerSocketChannel.open();
+		serverSocket.setOption(StandardSocketOptions.SO_REUSEADDR, true);
+		serverSocket.bind(new InetSocketAddress("localhost", port));
+		serverSocket.configureBlocking(false);
+		serverSocket.register(selector, SelectionKey.OP_ACCEPT);
+	}
+	
+	public void run() throws IOException {
+		while (true) {
+			selector.select();
+			Set<SelectionKey> selectedKeys = selector.selectedKeys();
+			Iterator<SelectionKey> iter = selectedKeys.iterator();
+			while (iter.hasNext()) {
+				SelectionKey key = iter.next();
+				if (key.isAcceptable()) {
+					SocketChannel clientSocket = ((ServerSocketChannel) key.channel()).accept();
+					clientSocket.configureBlocking(false);
+					clientSocket.register(selector, SelectionKey.OP_READ);
+					// TODO: Create connection object and call callback with it
+				} else if (key.isReadable()) {
+					ByteBuffer bb = ByteBuffer.allocate(1024);
+					int br = ((SocketChannel) key.channel()).read(bb);
+					if (br <= 0) {
+						key.cancel();
+						key.channel().close();
+						// TODO: Close connection object
+					}
+					System.out.println(new String(bb.array()));
+				}
+				iter.remove();
+			}
+		}
+	}
+	
+	public static void main(String[] args) throws IOException {
+		Server server = new Server();
+		server.serveOn(8888);
+		server.run();
+	}
+}
