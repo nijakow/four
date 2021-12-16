6231a219ae33126e701427e8e1a8598f1e273384
mhahn <mhahn@student.42heilbronn.de>
2021-12-16 18:29:25 +0100

    Adde basic CLI

--- src/nijakow/four/Four.java
+++ src/nijakow/four/Four.java
@@ -1,6 +1,7 @@
 package nijakow.four;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import nijakow.four.c.runtime.Key;
 import nijakow.four.c.runtime.fs.FSNode;
@@ -58,7 +59,43 @@ public class Four implements Runnable {
 	}
 	
 	public static void main(String[] args) throws IOException {
-		Four four = new Four(new int[] { 4242 });
+		ArrayList<Integer> ports = new ArrayList<>();
+		for (int i = 0; i < args.length; i++) {
+			switch (args[i]) {
+			case "--client":
+				// TODO
+				return;
+
+			case "--port":
+			case "-p":
+				int j = 0;
+				try {
+					for (; j < args.length - i; j++) {
+						ports.add(Integer.parseInt(args[++i]));
+					}
+				} catch (NumberFormatException | IndexOutOfBoundsException e) {
+					if (j == 0)
+						System.err.println("No proper port specified! Argument was: " + args[i]);
+				}
+				break;
+				
+			case "-h":
+				System.out.println("--port");
+				System.out.println("-p");
+				System.out.println("         Specifies a (or multiple) port(s) to use");
+				System.out.println("--client");
+				System.out.println("         Starts a client instance");
+				System.out.println("-h");
+				System.out.println("         Prints this help");
+				break;
+			}
+		}
+		if (ports.isEmpty())
+			ports.add(4242);
+		int[] ps = new int[ports.size()];
+		for (int i = 0; i < ps.length; i++)
+			ps[i] = ports.get(i);
+		Four four = new Four(ps);
 		four.run();
 	}
 }
