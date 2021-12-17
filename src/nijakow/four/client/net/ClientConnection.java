ca8ef49d0a84775785bcb389ae08cf0090cc57b9
mhahn <mhahn@student.42heilbronn.de>
2021-12-17 20:52:48 +0100

    Fixed error messages and reconnection tries in client

--- src/nijakow/four/client/net/ClientConnection.java
+++ src/nijakow/four/client/net/ClientConnection.java
@@ -4,4 +4,8 @@ public interface ClientConnection {
 	public void close();
 	public boolean isConnected();
 	public void send(String message);
+	
+	public static ClientConnection getClientConnection(String host, int port) {
+		return new ClientConnectionImpl(host, port);
+	}
 }
