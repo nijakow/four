6f3669321e0adb6b80f08a30c1ec74dd1e16811d
mhahn <mhahn@student.42heilbronn.de>
2021-12-17 16:51:42 +0100

    Added saving and using window bounds

--- src/nijakow/four/client/ClientWindow.java
+++ src/nijakow/four/client/ClientWindow.java
@@ -4,6 +4,8 @@ import java.awt.BorderLayout;
 import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
@@ -16,9 +18,12 @@ public class ClientWindow extends JFrame implements ActionListener {
 	private static final String SETTINGS = "settings";
 	private static final String SEND = "send";
 	private JTextField prompt;
+	private PreferencesHelper prefs;
 	
 	public ClientWindow() {
 		super("Nijakow's \"Four\"");
+		// TODO macOS customization
+		prefs = new PreferencesHelper();
 		getContentPane().setLayout(new BorderLayout());
 		JPanel south = new JPanel();
 		south.setLayout(new BoxLayout(south, BoxLayout.X_AXIS));
@@ -28,20 +33,42 @@ public class ClientWindow extends JFrame implements ActionListener {
 		JButton settings = new JButton("Settings");
 		settings.addActionListener(this);
 		settings.setActionCommand(SETTINGS);
-		south.add(settings);
 		south.add(prompt);
+		south.add(settings);
 		getContentPane().add(south, BorderLayout.SOUTH);
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		// TODO save constraints
-		pack();
-		setLocationRelativeTo(null);
-		setVisible(true);
+		setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+		addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent e) {
+				prefs.setWindowDimensions(getX(), getY(), getWidth(), getHeight());
+				prefs.flush();
+				System.out.println("TODO: Close connection");
+				dispose();
+			}
+		});
+		int x = prefs.getWindowPositionX();
+		int y = prefs.getWindowPositionY();
+		if (x == -1 || y == -1)
+			setLocationRelativeTo(null);
+		else
+			setLocation(x, y);
+		int width = prefs.getWindowWidth();
+		int height = prefs.getWindowHeight();
+		if (width == -1 || height == -1)
+			pack();
+		else
+			setSize(width, height);
+	}
+	
+	private void openSettingsWindow() {
+		
 	}
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		switch (e.getActionCommand()) {
 		case SETTINGS:
+			openSettingsWindow();
 			System.out.println("TODO: settings");
 			break;
 			
@@ -53,6 +80,7 @@ public class ClientWindow extends JFrame implements ActionListener {
 	}
 	
 	public static void openWindow() {
-		EventQueue.invokeLater(() -> new ClientWindow());
+		EventQueue.invokeLater(() -> new ClientWindow().setVisible(true));
+		// TODO connection stuff
 	}
 }
