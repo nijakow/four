bf2975f5a41e409de0399873b748fdc3232b99b6
nijakow <79372954+nijakow@users.noreply.github.com>
2021-12-21 17:14:35 +0100

    Restructured thing.c

--- src/nijakow/four/lib/std/thing.c
+++ src/nijakow/four/lib/std/thing.c
@@ -1,17 +1,62 @@
 inherit "/secure/object.c";
 
-string short;
-string long;
+object parent;
+object sibling;
+object children;
 
-string get_short() { return short; }
-string get_long()  { return long;  }
+object get_parent()   { return parent;  }
+object get_sibling()  { return sibling; }
+object get_children() { return children; }
 
-void set_short(string s) { short = s; }
-void set_long(string l)  { long  = l; }
+void remove_child(object child)
+{
+    if (children == child) {
+        children = child->get_sibling();
+    } else {
+        for (object c = children; c != nil; c = c->get_sibling)
+        {
+            if (c->get_sibling() == child) {
+                c->sibling = child->get_sibling();
+                break;
+            }
+        }
+    }
+}
+
+void move_to(object new_parent)
+{
+    if (parent != new_parent)
+    {
+        if (parent != nil) {
+            parent->remove_child(this);
+            parent = nil;
+        }
+    
+        if (new_parent != nil) {
+            parent = new_parent;
+            sibling = new_parent->children;
+            new_parent->children = this;
+        }
+    }
+}
+
+
+string short_desc;
+string long_desc;
+
+string get_short() { return short_desc; }
+string get_long()  { return long_desc;  }
+
+void set_short(string s) { short_desc = s; }
+void set_long(string l)  { long_desc  = l; }
+
 
 void create()
 {
     "/secure/object.c"::create();
+    parent = nil;
+    sibling = nil;
+    children = nil;
     set_short("<error>");
     set_long("<error>");
 }
