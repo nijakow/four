642e7c8729ab1f4ad881ae6414ada19bcccf951c
enijakow <79372954+nijakow@users.noreply.github.com>
2022-01-23 23:22:19 +0100

    Fixed bugs, fully moved over to ctrl.c

--- src/nijakow/four/lib/std/apps/ctrl/ctrl.c
+++ src/nijakow/four/lib/std/apps/ctrl/ctrl.c
@@ -3,6 +3,7 @@ inherits "/std/cli.c";
 mapping cmds;
 object me;
 
+
 object get_location()
 {
     return me->get_location();
@@ -23,6 +24,44 @@ void lookaround()
 	        if (obj != me)
 	            connection()->write(capitalize(obj->get_short()), ".\n");
 	    }
+    }
+}
+
+
+list select_object_list;
+func select_cb;
+
+void _select_choose(string line)
+{
+    int choice = atoi(line);
+
+    if ((choice == nil) || (choice < 1) || (choice > length(select_object_list)))
+        _select_resume();
+    else
+        call(select_cb, select_object_list[choice - 1]);
+}
+
+void _select_resume()
+{
+    connection()->write("\nYour selection is ambiguous:\n");
+    for (int i = 0; i < length(select_object_list); i++)
+    {
+        connection()->write("  ", (i + 1), ": ", select_object_list[i].get_long(), "\n");
+    }
+    connection()->prompt(this::_select_choose, "Please choose: ");
+}
+
+void select_and_call(string text, func cb)
+{
+    list objects = me->find_thing_here(text);
+    if (length(objects) == 0) {
+        call(cb, nil);
+    } else if (length(objects) == 1) {
+        call(cb, objects[0]);
+    } else {
+        select_object_list = objects;
+        select_cb          = cb;
+        _select_resume();
     }
 }
 
@@ -160,10 +199,16 @@ void add_cmd(string name, func cb)
     cmds[name] = cb;
 }
 
+void forward_write(...)
+{
+    connection()->write(...);
+}
+
 void create(object connection, func finish_cb, object me)
 {
     "/std/cli.c"::create(connection, finish_cb);
     connection->set_fallback(this::resume);
+    me->submit_lines_to(this::forward_write);
     this.cmds = [];
     this.me = me;
     add_cmd("look", this::cmd_look);
--- src/nijakow/four/lib/std/player.c
+++ src/nijakow/four/lib/std/player.c
@@ -1,49 +1,17 @@
 inherits "/std/thing.c";
 
+func line_cb;
+
 void write(...)
 {
-    // TODO
-    log("Player received a write: ", ...);
+    if (line_cb != nil)
+        call(line_cb, ...);
 }
 
-
-list select_object_list;
-func select_cb;
-
-void _select_choose(string line)
+void submit_lines_to(func cb)
 {
-    int choice = atoi(line);
-
-    if ((choice == nil) || (choice < 1) || (choice > length(select_object_list)))
-        _select_resume();
-    else
-        call(select_cb, select_object_list[choice - 1]);
+    this.line_cb = cb;
 }
-
-void _select_resume()
-{
-    connection->write("\nYour selection is ambiguous:\n");
-    for (int i = 0; i < length(select_object_list); i++)
-    {
-        connection->write("  ", (i + 1), ": ", select_object_list[i].get_long(), "\n");
-    }
-    connection->prompt(this::_select_choose, "Please choose: ");
-}
-
-void select_and_call(string text, func cb)
-{
-    list objects = find_thing_here(text);
-    if (length(objects) == 0) {
-        call(cb, nil);
-    } else if (length(objects) == 1) {
-        call(cb, objects[0]);
-    } else {
-        select_object_list = objects;
-        select_cb          = cb;
-        _select_resume();
-    }
-}
-
 
 void activate_as(string name)
 {
