d6462971a1294cce3a94d7b00a272f0bec27b831
enijakow <79372954+nijakow@users.noreply.github.com>
2022-01-23 11:57:46 +0100

    Extended /std/apps/ctrl/ctrl.c

--- src/nijakow/four/lib/std/apps/ctrl/ctrl.c
+++ src/nijakow/four/lib/std/apps/ctrl/ctrl.c
@@ -3,20 +3,25 @@ inherits "/std/cli.c";
 mapping cmds;
 object me;
 
+void get_location()
+{
+    return me->get_location();
+}
+
 void lookaround()
 {
-    connection->write("\n");
+    connection()->write("\n");
     if (query_light_level_here() == 0) {
-        connection->write("Is is pitch black here.\n");
+        connection()->write("Is is pitch black here.\n");
     } else {
-	    connection->write(get_location()->get_short(), "\n");
-	    connection->write(get_location()->get_desc(), "\n");
+	    connection()->write(get_location()->get_short(), "\n");
+	    connection()->write(get_location()->get_desc(), "\n");
 	    for (object obj = get_location()->get_children();
 	         obj != nil;
 	         obj = obj->get_sibling())
 	    {
 	        if (obj != this)
-	            connection->write(capitalize(obj->get_short()), ".\n");
+	            connection()->write(capitalize(obj->get_short()), ".\n");
 	    }
     }
 }
@@ -26,6 +31,25 @@ void cmd_look(string arg)
     lookaround();
     resume();
 }
+
+void cmd_go(string dir)
+{
+    object loc = get_location()->get_exit(dir);
+
+    if (loc != nil) {
+    	me->act_goto(loc);
+    } else {
+    	connection()->write("There is no exit in this direction!\n");
+    }
+    resume();
+}
+
+void cmd_say(string text)
+{
+    me->act(me->get_short(), " says: ", text, "\n");
+    resume();
+}
+
 
 void docmd(string cmd, string args)
 {
@@ -75,4 +99,6 @@ void create(object connection, func finish_cb, object me)
     this.cmds = [];
     this.me = me;
     add_cmd("look", this::cmd_look);
+    add_cmd("go", this::cmd_go);
+    add_cmd("say", this::cmd_say);
 }
