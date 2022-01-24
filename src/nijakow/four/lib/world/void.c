f2c0b0e08fb0b5afb479c4b811fc23b2537a9264
enijakow <79372954+nijakow@users.noreply.github.com>
2022-01-24 23:46:37 +0100

    Deleted /realms

--- src/nijakow/four/lib/world/void.c
+++ src/nijakow/four/lib/world/void.c
@@ -5,6 +5,5 @@ void create()
     "/std/room.c"::create();
     set_name("The Void");
     set_desc("You are floating in a dark, empty space.");
-    add_exit("up", "/realms/nijakow/workroom.c");
     add_exit("north", "/world/cafeteria.c");
 }
