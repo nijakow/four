146c4200787b48c0a94eb8276fbc7d98d6050cfc
nijakow <79372954+nijakow@users.noreply.github.com>
2022-01-27 19:10:42 +0100

    Fixed a bug in cc.c

--- src/nijakow/four/lib/sbin/cc.c
+++ src/nijakow/four/lib/sbin/cc.c
@@ -3,7 +3,7 @@ inherits "/std/app.c";
 void start()
 {
     if (length(argv) <= 1)
-        arg_error();
+        connection()->write("Argument error!\n");
     else {
         for (int i = 1; i < length(argv); i++)
         {
