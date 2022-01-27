c0a9ee3394ce23a891c0dd262f06e3d42c911e48
nijakow <79372954+nijakow@users.noreply.github.com>
2022-01-27 14:02:30 +0100

    Added /bin/new.c

---
+++ src/nijakow/four/lib/bin/new.c
@@ -0,0 +1,31 @@
+inherits "/std/cli.c";
+
+void start(list argv)
+{
+    if (length(argv) < 3)
+        connection()->write("Argument error!\n");
+    else {
+        object target = the(argv[1]);
+        if (target == nil)
+            connection()->write("Could not find the target!\n");
+        else
+        {
+            for (int i = 2; i < length(argv); i++)
+            {
+                string path = resolve(pwd(), argv[i]);
+                connection()->write("Instantiating ", path, "...\n");
+                if (path != nil)
+                {
+                    object obj = new(path);
+                    obj->move_to(target);
+                }
+            }
+        }
+    }
+    exit();
+}
+
+void create(object connection, func finish_cb)
+{
+    "/std/cli.c"::create(connection, finish_cb);
+}
