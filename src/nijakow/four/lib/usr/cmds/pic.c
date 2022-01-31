9123155cdd995d9eac082c1d43ceeea3210b9ff8
nijakow <79372954+nijakow@users.noreply.github.com>
2022-01-31 20:41:14 +0100

    Extended the 'pic' command

--- src/nijakow/four/lib/usr/cmds/pic.c
+++ src/nijakow/four/lib/usr/cmds/pic.c
@@ -1,12 +1,22 @@
 inherits "/std/cmd.c";
 
-void start()
+void examine(object the_object)
 {
-    string img = get_location()->get_img();
+    string img = nil;
+    if (the_object != nil)
+        img = the_object->get_img();
     if (img == nil) {
         connection()->write("There is no image available!\n");
     } else {
         connection()->write("\{^" + img + "\}\n");
     }
     exit();
+}
+
+void start()
+{
+    if (trim(arg()) == "")
+        examine(get_location());
+    else
+        select_and_call(arg(), this::examine);
 }
