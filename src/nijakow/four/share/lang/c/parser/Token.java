fda99102b2d5b539b91c7cbd64374aad041982c2
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2022-03-12 15:47:53 +0100

    Added end position to the tokens

--- src/nijakow/four/share/lang/c/parser/Token.java
+++ src/nijakow/four/share/lang/c/parser/Token.java
@@ -3,11 +3,15 @@ package nijakow.four.share.lang.c.parser;
 public class Token {
 	private final Tokenizer tokenizer;
 	private final StreamPosition position;
+	private final StreamPosition endPosition;
 	private final TokenType type;
 	private final Object payload;
 	
 	public StreamPosition getPosition() {
 		return position;
+	}
+	public StreamPosition getEndPosition() {
+		return endPosition;
 	}
 	
 	public boolean is(TokenType type) {
@@ -26,13 +30,14 @@ public class Token {
 		tokenizer.unread(this);
 	}
 	
-	public Token(Tokenizer tokenizer, StreamPosition pos, TokenType type) {
-		this(tokenizer, pos, type, null);
+	public Token(Tokenizer tokenizer, StreamPosition pos, StreamPosition endPos, TokenType type) {
+		this(tokenizer, pos, endPos, type, null);
 	}
 	
-	public Token(Tokenizer tokenizer, StreamPosition pos, TokenType type, Object payload) {
+	public Token(Tokenizer tokenizer, StreamPosition pos, StreamPosition endPos, TokenType type, Object payload) {
 		this.tokenizer = tokenizer;
 		this.position = pos;
+		this.endPosition = endPos;
 		this.type = type;
 		this.payload = payload;
 	}
