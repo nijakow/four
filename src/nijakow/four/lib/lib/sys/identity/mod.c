6ad0a54edf3d31b05f3591f087dc5d7ce3b134d4
nijakow <79372954+nijakow@users.noreply.github.com>
2022-04-03 18:42:18 +0200

    Added users and groups to the new standard library

---
+++ src/nijakow/four/lib/lib/sys/identity/mod.c
@@ -0,0 +1,24 @@
+use $adduser;
+use $deluser;
+use $addtogroup;
+use $removefromgroup;
+
+bool System_AddUser(string name)
+{
+    return $adduser(name);
+}
+
+bool System_DeleteUser(string name)
+{
+    return $deluser(name);
+}
+
+bool System_AddUserToGroup(string user, string group)
+{
+    return $addtogroup(user, group);
+}
+
+bool System_RemoveUserFromGroup(string user, string group)
+{
+    return $removefromgroup(user, group);
+}
