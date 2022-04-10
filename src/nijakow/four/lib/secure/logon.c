3fa3be48742bcc0d4d4524905eb62c5312bc2382
nijakow <79372954+nijakow@users.noreply.github.com>
2022-04-10 16:57:24 +0200

    Implemented /secure/logon.c

--- src/nijakow/four/lib/secure/logon.c
+++ src/nijakow/four/lib/secure/logon.c
@@ -1,7 +1,77 @@
 inherits "/lib/app.c";
 
+use $login;
+
+private string username;
+
+private void banner()
+{
+    printf("\n");
+    printf("nijakow and mhahnFr present...\n");
+    printf("\n");
+    printf("\n");
+    printf("                                  /####/  #################\n");
+    printf("                                /####/    #####/     ######\n");
+    printf("                              /####/      ###/       ######\n");
+    printf("                            /####/        #/         /####/\n");
+    printf("                          /####/          /        /####/\n");
+    printf("                        /####/                   /####/\n");
+    printf("                      /####/                   /####/\n");
+    printf("                    /####/                   /####/\n");
+    printf("                  /####/                   /####/         /\n");
+    printf("                  #####################   ######         /#\n");
+    printf("                  #####################   ######       /###\n");
+    printf("                  #####################   ######     /#####\n");
+    printf("                                 ######   #################\n");
+    printf("                                 ######\n");
+    printf("                                 ######\n");
+    printf("                                 ######\n");
+    printf("\n");
+    printf("  Welcome to the 42 MUD!\n");
+    printf("\n");
+    printf("\n");
+}
+
+private void identification_failure()
+{
+    printf("\n");
+    printf("Credentials not recognized.\n");
+    printf("\n");
+    ask_username();
+}
+
+private void launch_shell()
+{
+    exec(exit, "/bin/sh.c", []);
+}
+
+private void receive_password(string pass)
+{
+    if (!$login(this.username, pass)) {
+        identification_failure();
+    } else {
+        launch_shell();
+    }
+}
+
+private void ask_password()
+{
+    password(receive_password, "password: ");
+}
+
+private void receive_username(string username)
+{
+    this.username = username;
+    ask_password();
+}
+
+private void ask_username()
+{
+    prompt(receive_username, "login: ");
+}
+
 void main()
 {
-    printf("Welcome to the MUD!\n");
-    exec(exit, "/bin/sh.c", []);
+    banner();
+    ask_username();
 }
