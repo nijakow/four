eeba8a1202062fcf56d2dc8d9c3cc164f07f62a7
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2022-04-26 18:53:13 +0200

    Middle state of rewriting the highlighting in the editor

--- src/nijakow/four/client/editor/FDocument.java
+++ src/nijakow/four/client/editor/FDocument.java
@@ -1,7 +1,6 @@
 package nijakow.four.client.editor;
 
 import nijakow.four.client.utils.StringHelper;
-import nijakow.four.share.lang.c.ast.ASTClass;
 import nijakow.four.share.lang.c.parser.*;
 import nijakow.four.share.util.Pair;
 
@@ -17,16 +16,18 @@ public class FDocument extends DefaultStyledDocument {
     private boolean highlighting;
     private boolean autoIndenting;
     private FTheme theme;
-    private final List<Token> idents;
-    private final List<Pair<Integer, Integer>> ide;
+    private final List<Pair<Integer, Integer>> comments;
+    //private final List<Token> idents;
+    ///private final List<Pair<Integer, Integer>> ide;
     private final ExecutorService threads;
 
     public FDocument() {
         threads = Executors.newSingleThreadScheduledExecutor();
         def = getLogicalStyle(0);
         theme = new DefaultTheme();
-        idents = new ArrayList<>();
-        ide = new ArrayList<>();
+        comments = new ArrayList<>();
+//        idents = new ArrayList<>();
+//        ide = new ArrayList<>();
         highlighting = false;
         try {
             text = getText(0, getLength());
@@ -64,7 +65,7 @@ public class FDocument extends DefaultStyledDocument {
         if (!highlighting)
             resetHighlight();
         else
-            threads.execute(this::updateSyntaxHighlighting);
+            threads.execute(() -> updateSyntaxHighlighting2(0, getLength()));
     }
 
     public boolean isSyntaxHighlighting() {
@@ -79,26 +80,29 @@ public class FDocument extends DefaultStyledDocument {
         } catch (BadLocationException e) {
             e.printStackTrace();
         }
-        if (highlighting)
-            threads.execute(this::updateSyntaxHighlighting);
     }
 
     @Override
     public void insertString(int offs, String str, AttributeSet a) throws BadLocationException {
         if (autoIndenting) {
-            if (str.equals("}") && isOnlyWhitespacesOnLine(offs)) {
+            /*if (str.equals("}") && isOnlyWhitespacesOnLine(offs)) {
                 offs = fixIndentation(offs, true);
             } else if (str.equals("\n")) {
                 str += StringHelper.generateFilledString(' ', getIndentationLevel(offs) * 4);
             } else if (str.equals("\t") && isOnlyWhitespacesOnLine(offs)) {
                 fixIndentation(offs, false);
                 return;
-            }
+            }*/
         }
         if (str.equals("\t")) {
             str = "    ";
         }
         super.insertString(offs, str, a);
+        if (highlighting) {
+            String finalStr = str;
+            int finalOffs = offs;
+            threads.execute(() -> updateSyntaxHighlighting2(finalOffs, finalStr.length()));
+        }
     }
 
     @Override
@@ -111,6 +115,11 @@ public class FDocument extends DefaultStyledDocument {
             offs = lineStart;
         }
         super.remove(offs, len);
+        if (highlighting) {
+            int finalLen = len;
+            int finalOffs = offs;
+            threads.execute(() -> updateSyntaxHighlighting2(finalOffs, finalLen));
+        }
     }
 
     @Override
@@ -121,8 +130,6 @@ public class FDocument extends DefaultStyledDocument {
         } catch (BadLocationException e) {
             e.printStackTrace();
         }
-        if (highlighting)
-            threads.execute(this::updateSyntaxHighlighting);
     }
 
     public void resetHighlight() {
@@ -130,11 +137,12 @@ public class FDocument extends DefaultStyledDocument {
     }
 
     public List<FSuggestion> computeSuggestions(int cursorPosition) {
-        List<FSuggestion> ret = new ArrayList<>();
+        /*List<FSuggestion> ret = new ArrayList<>();
         for (Token t : idents) {
             ret.add(new FSuggestion(t.getPayload().toString()));
         }
-        return ret;
+        return ret;*/
+        return new ArrayList<>();
     }
 
     public int getLineEnd(int position) {
@@ -156,7 +164,7 @@ public class FDocument extends DefaultStyledDocument {
         return true;
     }
 
-    public int fixIndentation(int position, boolean isClosingBrace) throws BadLocationException {
+    /*public int fixIndentation(int position, boolean isClosingBrace) throws BadLocationException {
         int j, i = getLineStart(position);
         for (j = i; j < text.length() && Character.isWhitespace(text.charAt(j)) && text.charAt(j) != '\n'; j++);
         remove(i, j - i);
@@ -165,9 +173,9 @@ public class FDocument extends DefaultStyledDocument {
         if (newIndentation < 0) newIndentation = 0;
         insertString(i, StringHelper.generateFilledString(' ', newIndentation * 4), null);
         return i + newIndentation * 4;
-    }
+    }*/
 
-    public synchronized int getIndentationLevel(int indentationPosition) {
+    /*public synchronized int getIndentationLevel(int indentationPosition) {
         int indent = 0;
         for (Pair<Integer, Integer> p : ide) {
             if (indentationPosition <= p.getFirst())
@@ -175,9 +183,50 @@ public class FDocument extends DefaultStyledDocument {
             indent = p.getSecond();
         }
         return indent;
-    }
+    }*/
 
     public void updateSyntaxHighlighting() {
+        updateSyntaxHighlighting2(0, getLength());
+    }
+
+    public void updateSyntaxHighlighting2(int offset, int length) {
+        // TODO Generate comment list
+        for (Pair<Integer, Integer> c : comments) {
+            boolean start = false,
+                    end = false;
+            if (offset > c.getFirst() && offset < c.getSecond()) {
+                start = true;
+            }
+            if (offset + length > c.getFirst()) {
+                end = true;
+            }
+            if (start && end) return;
+            if (start) {
+                offset = c.getSecond();
+            }
+            if (end) {
+                length -= (offset + length) - c.getFirst();
+            }
+        }
+        try {
+            final int lineStart = getLineStart(offset);
+            final int lineEnd = getLineEnd(offset + length);
+            String text = this.text.substring(lineStart, lineEnd);
+            Tokenizer tokenizer = new Tokenizer(new StringCharStream("", text));
+            tokenizer.enableCommentTokens();
+            Token token;
+            do {
+                token = tokenizer.nextToken();
+                Style style = theme.getStyle(token.getType()) == null ? def : theme.getStyle(token.getType()).asStyle(def);
+                if (style == null) style = def;
+                int pos = token.getPosition().getIndex() + lineStart;
+                setCharacterAttributes(pos, (token.getEndPosition().getIndex() + lineStart) - pos, style, true);
+            } while (token.getType() != TokenType.EOF);
+        } catch (ParseException e) {
+        }
+    }
+
+    /*public void updateSyntaxHighlightingOld() {
         try {
             idents.clear();
             List<Pair<Integer, Integer>> ideLocal = new ArrayList<>();
@@ -212,5 +261,5 @@ public class FDocument extends DefaultStyledDocument {
             if (t != null)
                setCharacterAttributes(t.getPosition().getIndex(), t.getEndPosition().getIndex() - t.getPosition().getIndex(), s, false);
         }
-    }
+    }*/
 }
