f522a07536a0e3dcfef280e19e4337381f933f54
nijakow <79372954+nijakow@users.noreply.github.com>
2022-05-11 20:19:18 +0200

    Extended thing.c

--- src/nijakow/four/lib/std/thing.c
+++ src/nijakow/four/lib/std/thing.c
@@ -1,4 +1,9 @@
 #include "/lib/object.c"
+#include "/lib/list/list.c"
+
+/*
+ *     L o c a t i o n   S e c t i o n
+ */
 
 use $get_parent;
 use $get_sibling;
@@ -14,15 +19,42 @@ void move_to(object target)
     $move_to(target);
 }
 
+/*
+ *     N a m e   S e c t i o n
+ */
+
+private string short_description;
+private string long_description;
+private string* identifiers;
+
+string get_short() { return this.short_description; }
+void set_short(string new_short) { this.short_description = new_short; }
+
+string get_long() { return this.long_description; }
+void set_long(string new_long) { this.long_description = new_long; }
+
+void add_id(string id) { List_Append(identifiers, id); }
+
+/*
+ *     C o m m a n d   S e c t i o n
+ */
+
 bool obey(string command)
 {
     return false;
 }
 
+/*
+ *     E v e n t   S e c t i o n
+ */
+
 void receive(string event)
 {
 }
 
+/*
+ *     C o n s t r u c t o r   S e c t i o n
+ */
 void reset()
 {
 }
@@ -30,4 +62,6 @@ void reset()
 void _init()
 {
     "/lib/object.c"::_init();
+    set_short("a thing");
+    this.identifiers = {};
 }
