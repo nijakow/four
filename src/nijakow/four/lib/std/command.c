2cbc282eabc24e5cc0fb9594f31c31125253d4a4
nijakow <79372954+nijakow@users.noreply.github.com>
2022-05-14 22:48:48 +0200

    Partially implemented /std/command.c

---
+++ src/nijakow/four/lib/std/command.c
@@ -0,0 +1,52 @@
+#include "/lib/object.c"
+#include "/lib/char/char.c"
+
+private string pattern;
+
+private bool match_loop(string pattern, string value, int a, int b)
+{
+    while (true)
+    {
+        if (a >= this.pattern->length) return true;
+        else if (b >= value.length) return false;
+
+        if (Char_IsSpace(this.pattern[a])) {
+            if (Char_IsSpace(value[b])) b = b + 1;
+            else a = a + 1;
+        }
+        else if (this.pattern[a] == '%') {
+            if (match_loop(pattern, value, a + 1, b))
+                return true;
+            // TODO
+            return match_loop(pattern, value, a, b + 1);
+        }
+        else if (this.pattern[a] == value[b]) {
+            a = a + 1;
+            b = b + 1;
+        }
+        else {
+            return false;
+        }
+    }
+}
+
+bool match(string value)
+{
+    if (this.pattern == nil)
+        return false;
+    return match_loop(this.pattern, value, 0, 0);
+}
+
+void set_pattern(string pattern)
+{
+    this.pattern = pattern;
+}
+
+void execute(mapping params)
+{
+}
+
+void _init()
+{
+    this.pattern = nil;
+}
